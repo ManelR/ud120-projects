96414145950148f893d71160c5da0f02cf323ac8
MRR <mrr@MacBook-Pro-de-MRR.local>
2017-03-04 20:35:42 +0100

    S'ha afegit la solució del kmeans

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -13,7 +13,8 @@ import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+from sklearn.cluster import KMeans
+import numpy as np
 
 
 
@@ -48,29 +49,43 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
-
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+
+#scaling
+minvalue=999999999999999
+maxvalue=0.0
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+for key, value in data_dict.iteritems():
+    if value["salary"] > maxvalue and value["salary"] != 'NaN':
+        maxvalue = value["salary"]
+    if value["salary"] < minvalue and value["salary"] != 'NaN':
+        minvalue = value["salary"]
+
+print maxvalue
+print minvalue
+
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
-
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred = kmeans.labels_
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters2.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
